b7d58403f4ce1f0ef9beb3b5f9923a447139907f
Admin <admin@macmini.local>
2018-02-11 12:27:02 +0530

    Initial Commit

---
+++ iOSExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSExercise
+//
+//  Created by Admin on 11/02/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSExercise/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOSExercise
+//
+//  Created by Admin on 11/02/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

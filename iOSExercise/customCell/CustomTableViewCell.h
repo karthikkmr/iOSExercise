adc21ad6ef60675e128f54e1f7963c1f9855d851
Admin <admin@MacMini.local>
2018-02-13 00:16:43 +0530

    base sourcecode

---
+++ iOSExercise/customCell/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  iOSExercise
+//
+//  Created by Admin on 11/02/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@end
---
+++ iOSExercise/customCell/CustomTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.m
+//  iOSExercise
+//
+//  Created by Admin on 11/02/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import "CustomTableViewCell.h"
+
+@implementation CustomTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
